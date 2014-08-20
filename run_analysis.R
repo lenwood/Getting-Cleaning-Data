c87f216a6d99ae8469a529d2a8a1b0a65d848d43
Chris Leonard <lenwood.cc@gmail.com>
2014-08-19 22:33:00 -0400

    complete first 4 assignments

--- run_analysis.R
+++ run_analysis.R
@@ -15,3 +15,54 @@ testSubject <- read.table("UCI HAR Dataset/test/subject_test.txt", sep="", nrow=
 trainSubject <- read.table("UCI HAR Dataset/train/subject_train.txt", sep="", nrow=7400)
 activities <- read.table("UCI HAR Dataset/activity_labels.txt", sep="", header=FALSE)
 features <- read.table("UCI HAR Dataset/features.txt", sep="", header=FALSE, nrow=600)
+
+
+## 1.
+# Merge test & train into one data set.
+data <- rbind(test_x, train_x)
+
+rm(test_x, train_x)
+
+
+## 4. Set variable names according to features.txt
+features$V1 <- NULL
+features <- t(features)
+names(data) <- features
+rm(features)
+
+
+## 2. Extract mean & standard deviation for each measurements.
+mean <- grep("mean()", colnames(data), fixed=TRUE)
+SD <- grep("std()", colnames(data), fixed=TRUE)
+
+data <- data[, c(mean, SD)]
+rm(mean, SD)
+
+
+## 3. Add activity labels
+# add the labels to the dataset
+data_y <- rbind(test_y, train_y)
+rm(test_y, train_y)
+names(data_y) <- "label"
+
+data <- cbind(data, data_y)
+rm(data_y)
+
+activities$V1 <- NULL
+activities <- t(activities)
+data$activityName <- as.factor(data$label)
+levels(data$activityName) <- activities
+rm(activities)
+data$label <- NULL
+
+# add the subject to the data frame
+subject <- rbind(testSubject, trainSubject)
+names(subject) <- "subject"
+data <- cbind(data, subject)
+data$subject <- as.factor(data$subject)
+rm(subject, testSubject, trainSubject)
+
+# reorder columns
+data <- data[, c(68:67, 1:66)]
+
+
